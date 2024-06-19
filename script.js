3920c0acafecec7a22b41c7629221224a35f6b2b
EricaReibaldi <152083327+EricaReibaldi@users.noreply.github.com>
2024-06-19 15:56:18 +0000

    container

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 // scripts.js
 
-    function popup() {
-        var popup = document.getElementById("popup");
-        if (popup.style.display == "block") {
-            popup.style.display = "none";
-        } else {
-            popup.style.display = "block";
-        }
+function popup() {
+    var popup = document.getElementById("popup");
+    if (popup.style.display == "block") {
+        popup.style.display = "none";
+    } else {
+        popup.style.display = "block";
     }
+}
+
+
 
